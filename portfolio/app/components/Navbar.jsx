4e553d7c7a79871d15ab739cf2b46780ec51cefa
Sumnayak-Dev007 <nsumankumari225@gmail.com>
2025-09-09 21:12:22 +0530

    Rem submodule

---
+++ portfolio/app/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+
+const Navbar = () => {
+  return (
+    <>
+     <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex item-center justify-between z-50'>
+            {/* <a href="">
+            <Image src={assets.logo} className='w-28
+            cursor-pointer mr-14'/>
+            </a> */}
+
+            <ul className='hidden md:flex item-cener gap-6 lg:gap-8 rounded-full px-12 py-3'>
+            <li><a href="#top">Home</a></li>
+            <li><a href="#about">About me</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#work">My Work</a></li>
+            <li><a href="#contact">Contact me</a></li>
+            </ul>
+
+            <div>
+<a href="#contact" className='hidden  lg:flex items-center  gap-3  px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contact <Image alt='' src={assets.arrow_icon} className='w-3'/></a>
+</div>
+   </nav>
+    </>
+  )
+}
+
+export default Navbar
