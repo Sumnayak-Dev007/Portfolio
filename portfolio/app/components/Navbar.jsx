f0aca5d10c9051d560500aad80d6742af13161ee
Sumnayak-Dev007 <nsumankumari225@gmail.com>
2025-09-28 17:56:09 +0530

    Sumanayak.

--- portfolio/app/components/Navbar.jsx
+++ portfolio/app/components/Navbar.jsx
@@ -30,8 +30,8 @@ const Navbar = () => {
     </div> */}
      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex item-center justify-between z-50 bg-[#11001F] backdrop-blur-md ${isScroll?"border-b border-white/50 border-b-[0.2px] shadow-sm":""}`}>
             <a href="">
-            <Image src={assets.logo} className='w-28
-            cursor-pointer mr-14'/>
+            <Image src={assets.logo} className='w-40
+            cursor-pointer mr-14 '/>
             </a>
 
             
@@ -48,16 +48,16 @@ const Navbar = () => {
               {/* <button>
                 <Image src={assets.moon_icon} alt='' className='w-6 relative right-11 top-2'/>
               </button> */}
-          <a href="#contact" className='hidden  lg:flex items-center  gap-3  px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contact <Image alt='' src={assets.arrow_icon} className='w-3'/></a>
+          <a href="#contact" className='hidden  lg:flex items-center  gap-3  px-10 py-2.5 border border-white/50 rounded-full ml-4'>Contact <Image alt='' src={assets.arrow_icon_dark} className='w-3'/></a>
           <button className='block absolute top-6 right-6 md:hidden ml-3' onClick={openMenu}>
-            <Image src={assets.menu_black} alt='' className='w-6'/>
+            <Image src={assets.menu_white} alt='' className='w-6'/>
           </button>
 </div>
       {/* _________Mobile Menu _________ */}
 
-      <ul ref={sideMenuRef} className='flex md:hidden flex-col py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+      <ul ref={sideMenuRef} className='flex md:hidden flex-col py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen transition duration-500 bg-[var(--dark-hover)] text-white'>
         <div className='absolute right-6 top-6' onClick={closeMenu}>
-          <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
+          <Image src={assets.close_white} alt='' className='w-5 cursor-pointer'/>
         </div>
         <li><a className='font-ovo' onClick={closeMenu} href="#top">Home</a></li>
             <li><a className='font-ovo' onClick={closeMenu} href="#about">About me</a></li>
