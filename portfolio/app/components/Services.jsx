7fb75b96c087d4d259670c25fc3e5742a7d0baa0
Sumnayak-Dev007 <nsumankumari225@gmail.com>
2025-10-14 22:06:07 +0530

    Optimizing the UX

--- portfolio/app/components/Services.jsx
+++ portfolio/app/components/Services.jsx
@@ -5,7 +5,7 @@ import {motion} from "motion/react"
 
 const Services = () => {
   return (
-    <div id='services' className='w-full  px-[12%] py-10 scroll-mt-20 ' >
+    <div id='services' className='w-full  px-[8%] py-10 scroll-mt-20 ' >
       <h4 className='text-center mb-2 text-lg font-ovo'> What I Offer</h4>
       <motion.div
               className="text-center mb-16"
@@ -14,7 +14,7 @@ const Services = () => {
               transition={{ duration: 0.5, ease: "easeOut" }}
               viewport={{ once: true, margin: "-50px" }}
             >
-              <h2 className="text-3xl md:text-5xl font-bold mb-4 font-ovo">
+              <h2 className="text-3xl md:text-5xl font-bold  mb-4 font-ovo">
                 My <span className="text-green-500">Services</span>
               </h2>
               <p className='text-gray-400 max-w-2xl mx-auto text-sm sm:text-base'>
@@ -28,8 +28,8 @@ const Services = () => {
           className='border border-gray-400 rounded-lg px-8  py-12 hover-shadow-white cursor-pointer hover:bg-[var(--dark-hover)] hover:-translate-y-1 duration-500'
           >
             <Image src={icon} alt='' className='w-10'/>
-            <h3 className='text-lg my-4 text-white'>{title}</h3>
-            <p className='text-sm text-white/80 leading-5'>{description}</p>
+            <h3 className='my-4 text-[20px] text-white'>{title}</h3>
+            <p className='services-list-p '>{description}</p>
             <a href={link} className='flex items-center gap-2 text-sm mt-5'>Read more
               <Image src={assets.right_arrow} alt='' className='w-4' />
             </a>
